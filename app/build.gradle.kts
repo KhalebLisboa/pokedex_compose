251ca95f52f124237af0318b537bd84b82d435b1
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2026-03-14 17:33:55 -0400

    add: retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.hilt.android)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
