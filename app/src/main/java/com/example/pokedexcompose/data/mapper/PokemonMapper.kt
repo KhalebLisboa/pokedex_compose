62448157f7c34053f16c7cd81bc5fba67965e749
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2026-04-14 22:41:32 -0400

    config: add interceptor to improve testability in network requests

--- app/src/main/java/com/example/pokedexcompose/data/mapper/PokemonMapper.kt
+++ app/src/main/java/com/example/pokedexcompose/data/mapper/PokemonMapper.kt
@@ -5,7 +5,7 @@ import com.example.pokedexcompose.domain.model.Pokemon
 
 fun PokemonResultDto.toDomain(): Pokemon {
     val id = url.split("/").last { it.isNotEmpty() }
-    val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
+    val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
     return Pokemon(
         name = name.replaceFirstChar { it.uppercase() },
         url = url,
